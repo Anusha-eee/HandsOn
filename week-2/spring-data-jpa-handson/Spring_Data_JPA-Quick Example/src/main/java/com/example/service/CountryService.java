030ac2deeb7026c108df4e58c2c606e4b932635a
Anusha-eee <anushachinna16@gmail.com>
2026-07-08 22:51:50 +0530

    Added week-2

---
+++ week-2/spring-data-jpa-handson/Spring_Data_JPA-Quick Example/src/main/java/com/example/service/CountryService.java
@@ -0,0 +1,22 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import com.example.model.Country;
+import com.example.repository.CountryRepository;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
